4f404e3bac628b5c8cc06b61d77d620f454e2c68
Vale Quiroga <valentinqramos@outlook.com>
2022-05-15 20:45:12 -0500

    [200~styling the buttons

--- js/defaultVars.js
+++ js/defaultVars.js
@@ -4,30 +4,59 @@ const ctx = canvas.getContext('2d');
 let frames = 0
 let points = 0
 let clicks = 0
-let bomb = 0
 
-let mouseX = 0
-let mouseY = 0
+let venomCounter = 0
+let iceCounter = 0
+let rockCounter = 0
+let extraDamage = 0
+// let mouseX = 0
+// let mouseY = 0
+let requestId;
 
-const arrows = []
 const enemies = []
 
-//arrow types!
+//arrow types
+const arrows = []
 let regular = '/images/heroe/arrow/Move.png'
-let venom = '/images/heroe/arrow/venom.png.png'
-let ice = '/images/heroe/arrow/ice.png'
-let rock = '/images/heroe/arrow/rock.png'
+let venomArrow = '/images/heroe/arrow/venom.png'
+let iceArrow = '/images/heroe/arrow/ice.png'
+let rockArrow = '/images/heroe/arrow/rock.png'
 
 let arrowStyle = regular
 
 
+//monster speed
+let regularSpeed = 1
+let slowSpeed = 1
+let monsterSpeed = regularSpeed
 
-let requestId;
 
-/* 
-let audio = new Audio()
-audio.src = 
-*/
+// sounds 
+let arrowSound = new Audio()
+regularSound = '/audio/arrow.wav'
+iceSound = '/audio/ice.wav'
+rockSound = '/audio/rock.wav'
+venomSound = '/audio/venom.wav'
+arrowSound.src = regularSound
+
+
+//buttons style
+let black = 'rgb(25,25,25)'
+
+let venomButton = document.querySelector('#venom')
+let purple = 'rgb(115, 80, 115)'
+venomButton.style.backgroundColor = purple
+
+let iceButton = document.querySelector('#ice')
+let blue = 'rgb(70, 135, 175)'
+iceButton.style.backgroundColor = blue
+
+
+let rockButton = document.querySelector('#rock')
+let brown = 'rgb(90, 85, 85)'
+rockButton.style.backgroundColor = 'rgb(125, 100, 75)'
+rockButton.style.backgroundColor = brown
+
 
 //heroe default
 let idle = [
@@ -52,3 +81,5 @@ let enemyWalk = [
 ]
 
 
+const funcionara = new Image()
+  funcionara.src = '/images/backgrounds/gameOver.png'
--- js/script.js
+++ js/script.js
@@ -1,13 +1,12 @@
+
 window.onload = () => {
 
   const bg = new Background('images/backgrounds/background.png')
   const snowBlur1 = new BgSnow('/images/backgrounds/snow3.png',0.4)
   const snowBlur2 = new BgSnow('/images/backgrounds/snow11.png',0.2)
   const heroe = new Heroe(idle,-20,170,300,300)
-  const enemy = new Enemy(enemyWalk,canvas.height +40,40,500,500,0.03)
+  const enemy = new Enemy(enemyWalk,canvas.height +40,40,500,500,monsterSpeed)
   
-
-
   
 
   addEventListener('keydown',(event) =>{
@@ -21,12 +20,23 @@ window.onload = () => {
     requestId = requestAnimationFrame(updateGame)
   }
   
-  function gameOver(){
-    fillRect(0,0,canvas.width,canvas.height)
+  function gameOver(){ 
+    ctx.drawImage(funcionara,150,150,500,250)
     requestId = undefined
   }
 
-  function winGame(){} //-----------------------
+  function winGame(){
+    ctx.globalAlpha = 0.3
+    ctx.fillStyle = 'beige'
+    ctx.fillRect(50,50,canvas.width -100,canvas.height -100)
+
+    ctx.globalAlpha = 1
+    ctx.fillStyle = 'crimson'
+    ctx.font = '40px Roboto'
+    ctx.fillText('Congratulations YOU WIN!',180,400)
+
+    requestId = undefined
+  }
 
   function updateGame(){
     frames++
@@ -37,10 +47,8 @@ window.onload = () => {
     snowBlur2.draw()
     heroe.draw()
     enemy.draw()
-
     drawEnemies()
     generateArrows()
-
     testingLife()
 
     if(requestId){requestAnimationFrame(updateGame)}
@@ -49,24 +57,26 @@ window.onload = () => {
   function generateEnemies(){} //---------------
 
   function testingLife(){
+    let red = clicks + extraDamage
+
     ctx.fillStyle = 'black'
-    ctx.fillRect(195,495,410,35)
+    ctx.fillRect(195,15,410,35)
     ctx.fillStyle = 'limegreen';
-    ctx.fillRect(200,500,400,25)
+    ctx.fillRect(200,20,400,25)
     ctx.fillStyle = 'orangered';
-    ctx.fillRect(200,500,clicks + bomb,25)
+    ctx.fillRect(200,20,red,25)
+
+    if (red >= 400){winGame()}   
   }
 
   function generateArrows(){
     arrows.forEach((arrow,indexOfArrow) => {
-
       arrow.draw()
 
       if(enemy.collision(arrow)){
-        console.log('hit');
+        // console.log('hit');
       }
     })
-
   }
 
   function drawEnemies(){
@@ -76,7 +86,7 @@ window.onload = () => {
     }
   }
 
-  function stats(){} //----------------
+  function stats(){}//-------------------- 
   
   addEventListener('keydown',event => {
     event.preventDefault()
@@ -85,20 +95,71 @@ window.onload = () => {
   })
 
   addEventListener('click', () => {
-    clicks += 1
+    event.preventDefault()
+    clicks += 20
    arrows.push(new Arrow(80,280,13,arrowStyle))
-   console.log(clicks);
    arrowStyle = regular
+   monsterSpeed = regular
+
   })
 
+  document.getElementById('ice').onclick = () => {
+    if(iceButton.style.backgroundColor === blue){
+      extraDamage += 0.5
+      arrowStyle = iceArrow
+      iceCounter++
+      arrowSound.src = iceSound
+      arrowSound.play()
+    }
+
+    iceButton.style.backgroundColor = black
+    setTimeout(() => {
+      iceButton.style.backgroundColor = blue
+    },'5000')
+  }
+
+  document.getElementById('venom').onclick = () => {
+    if(venomButton.style.backgroundColor === purple){
+       extraDamage += 1
+       arrowStyle = venomArrow
+       venomCounter++
+       arrowSound.src = venomSound
+       arrowSound.play()
+    }
+
+    venomButton.style.backgroundColor = black
+    setTimeout(() => {
+       venomButton.style.backgroundColor = purple
+    },'7000')
+  }
+  
+  
+  document.getElementById('rock').onclick = () => {
+    if(rockButton.style.backgroundColor === brown){
+      extraDamage += 3
+      arrowStyle = rockArrow
+      rockCounter++
+      arrowSound.src = rockSound
+      arrowSound.play()
+
+      rockButton.style.backgroundColor = black
+      setTimeout(() => {
+        rockButton.style.backgroundColor = brown
+      },'10000')
+    }
+  }
+
+  addEventListener('keydown',event => {
+    event.preventDefault()
+    if(event.keyCode === 90){gameOver()} //z 2 veces
+    if(event.keyCode === 88){winGame()}  //x 2 veces
+  })
 };
 
- document.getElementById('test').onclick = () => {
-  venom += 0.5
-  arrowStyle = ice
- }
+ 
 
-//temas de gravedad en la clase 02:32
+
+
 //temas de generar pipes en la clase 03:01
 //borrar elementos que se salieron del canvas 03:33 y 03:37 activar gameOver con choques
 //colocar el gameover 04:03
